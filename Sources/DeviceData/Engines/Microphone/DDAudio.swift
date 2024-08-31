cc99de19bdb87fcb4538eab61f6d1bef0f3f3ac8
Anton Heestand <anton.heestand@gmail.com>
2024-08-31 12:17:12 +0900

    Audio Fix

--- Sources/DeviceData/Engines/Microphone/DDAudio.swift
+++ Sources/DeviceData/Engines/Microphone/DDAudio.swift
@@ -2,7 +2,7 @@ import CoreGraphics
 
 public struct DDAudio {
     public var amplitude: CGFloat {
-        1.0 - pow(10.0, averagePower / 20.0)
+        pow(10.0, averagePower / 20.0)
     }
     public let averagePower: CGFloat
     public let peakPower: CGFloat
