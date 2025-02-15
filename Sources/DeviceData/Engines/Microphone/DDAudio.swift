f94ddfdf5b98bb32804a30eb53e0994d38fe827f
Anton Heestand <anton.heestand@gmail.com>
2025-02-15 14:33:27 +0900

    Node Fixes

--- Sources/DeviceData/Engines/Microphone/DDAudio.swift
+++ Sources/DeviceData/Engines/Microphone/DDAudio.swift
@@ -1,8 +1,14 @@
 import CoreGraphics
 
 public struct DDAudio: Hashable {
+    @available(*, deprecated, renamed: "normalizedPower")
     public var amplitude: CGFloat {
         pow(10.0, averagePower / 20.0)
+    }
+    public var normalizedPower: CGFloat {
+        let minDb: CGFloat = -80.0
+        let clamped = max(averagePower, minDb)
+        return (clamped - minDb) / -minDb
     }
     public let averagePower: CGFloat
     public let peakPower: CGFloat
