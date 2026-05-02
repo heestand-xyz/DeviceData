0cb0187738374e415c401ea7f996b23493d5a247
Anton Heestand <anton.heestand@gmail.com>
2026-05-02 12:45:59 +0900

    Normalized Level

--- Sources/DeviceData/Engines/Microphone/DDAudio.swift
+++ Sources/DeviceData/Engines/Microphone/DDAudio.swift
@@ -1,7 +1,6 @@
 import CoreGraphics
 
 public struct DDAudio: Hashable {
-    @available(*, deprecated, renamed: "normalizedPower")
     public var amplitude: CGFloat {
         pow(10.0, averagePower / 20.0)
     }
@@ -9,6 +8,14 @@ public struct DDAudio: Hashable {
         let minDb: CGFloat = -80.0
         let clamped = max(averagePower, minDb)
         return (clamped - minDb) / -minDb
+    }
+    public var normalizedLevel: CGFloat {
+        let minDb: CGFloat = -80.0
+        let clamped = min(max(averagePower, minDb), 0.0)
+        let minAmplitude = pow(10.0, minDb / 20.0)
+        let amplitude = pow(10.0, clamped / 20.0)
+        let normalizedAmplitude = (amplitude - minAmplitude) / (1.0 - minAmplitude)
+        return pow(min(max(normalizedAmplitude, 0.0), 1.0), 0.5)
     }
     public let averagePower: CGFloat
     public let peakPower: CGFloat
